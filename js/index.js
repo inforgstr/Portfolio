b4bfb66678b32dce90eac5823ceb4bb89826d5ed
inforgstr <xboyxmax@gmail.com>
2023-08-03 11:21:28 +0500

    theme changed and refactored js code

--- js/index.js
+++ js/index.js
@@ -8,7 +8,25 @@ function displayButton(scrollPos, btn) {
 
 
 document.addEventListener("DOMContentLoaded", () => {
-    let scrollBtn = document.querySelector(".button-scroll-up");
+    var scrollBtn = document.querySelector(".button-scroll-up");
+    var moonBtn = document.querySelector(".navbar__side #moon");
+    var sunBtn = document.querySelector(".navbar__side #sun");
+    var styleLink = document.getElementById("theme-style");
+    var mainLine = document.getElementById("wave1");
+
+    moonBtn.addEventListener("click", () => {
+        styleLink.href = "./css/dark.css";
+        mainLine.setAttribute("style", "display:none");
+        moonBtn.setAttribute("style", "display:none");
+        sunBtn.setAttribute("style", "display:initial");
+    });
+
+    sunBtn.addEventListener("click", () => {
+        styleLink.href = "./css/default.css";
+        mainLine.setAttribute("style", "display:inherit;transform:rotate(180deg)");
+        sunBtn.setAttribute("style", "display:none");
+        moonBtn.setAttribute("style", "display:initial");
+    });
 
     document.addEventListener("scroll", event => {
         let lastScrollPosition = window.scrollY;
