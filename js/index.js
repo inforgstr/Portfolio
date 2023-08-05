abe7b468b561bb2077fa05097ce0ceb18370dfeb
inforgstr <xboyxmax@gmail.com>
2023-08-06 02:13:51 +0500

    upd: fixed texts sizes

--- js/index.js
+++ js/index.js
@@ -1,14 +1,4 @@
-function displayButton(scrollPos, btn) {
-    if (scrollPos >= 300) {
-        btn.style.display = "block";
-    } else {
-        btn.style.display = "none";
-    }
-};
-
-
 document.addEventListener("DOMContentLoaded", () => {
-    var scrollBtn = document.querySelector(".button-scroll-up");
     var moonBtn = document.querySelector(".navbar__side #moon");
     var sunBtn = document.querySelector(".navbar__side #sun");
     var styleLink = document.getElementById("theme-style");
@@ -26,16 +16,5 @@ document.addEventListener("DOMContentLoaded", () => {
         mainLine.setAttribute("style", "display:inherit;transform:rotate(180deg)");
         sunBtn.setAttribute("style", "display:none");
         moonBtn.setAttribute("style", "display:initial");
-    });
-
-    document.addEventListener("scroll", event => {
-        let lastScrollPosition = window.scrollY;
-        window.requestAnimationFrame(() => {
-            displayButton(lastScrollPosition, scrollBtn);
-        });
-    });
-
-    scrollBtn.addEventListener("click", event => {
-        window.scrollTo(0, 0)
     });
 });
